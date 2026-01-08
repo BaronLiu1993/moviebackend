cbab3c1660ee074a23684039761d1fbd13f2a3fb
Jie Xuan Liu <baronliu1993@gmail.com>
2026-01-07 21:30:00 -0500

    implemented query router

--- router/query/queryRouter.ts
+++ router/query/queryRouter.ts
@@ -1,6 +1,23 @@
 import { Router } from "express";
+import { getRecommendedFilms } from "../../service/query/queryService.js";
+import { verifyToken } from "../../middleware/verifyToken.js";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
 const router = Router();
+
+router.get("/", verifyToken, async (req, res) => {
+    const { search } = req.body
+    const supabaseClient = req.supabaseClient
+    if (!supabaseClient || !search) {
+        return res.status(400).json({message: "Missing Parameters"})
+    }
+    try {
+        const data = await getRecommendedFilms({supabaseClient})
+        return res.status(200).json({ data })      
+    } catch {
+        return res.status(500).json({message: "Internal Server Error"})
+    }
+})
 
 
 
