feff6503062c4e7e8836b1d18606b29b523c72e8
Jie Xuan Liu <baronliu1993@gmail.com>
2026-01-08 22:45:24 -0500

    implementing friends

--- service/auth/authService.ts
+++ service/auth/authService.ts
@@ -1,6 +1,8 @@
 import { createSignInSupabase } from "../supabase/configureSupabase.js";
 import OpenAI from "openai";
 import dotenv from "dotenv"
+import type { UUID } from "node:crypto";
+import type { SupabaseClient } from "@supabase/supabase-js";
 
 dotenv.config()
 
@@ -11,9 +13,26 @@ type ProfileChangeType = {
   inputString: string
 }
 
+type SelectProfileType = {
+  userId: UUID
+  friendId: UUID
+  supabaseClient: SupabaseClient
+}
+
+type FriendRequest
+
+
 const OpenAIClient = new OpenAI({
   apiKey: OPENAI_KEY,
 });
+
+const checkIsFriends = async ({supabaseClient, userId, friendId}: SelectProfileType) => {
+  const {data, error }  = await supabaseClient.rpc("is_following", {
+    p_following_id: friendId,
+    p_follower_id: userId
+  })
+  return data as boolean
+}
 
 export const handleSignIn = async (): Promise<string> => {
   const supabase = createSignInSupabase();
@@ -39,4 +58,45 @@ export const generateInterestProfileVector = async ({inputString}: ProfileChange
     encoding_format: "float"
   })
   return response.data[0]?.embedding
+}
+
+export const sendFriendRequest = async ({userId, friendId, supabaseClient}) => {
+  
+}
+
+export const acceptFriendRequest = async ({userId, friendId, supabaseClient}) => {
+
+}
+
+export const rejectFriendRequest = async ({userId, friendId, supabaseClient}) => {
+
+}
+
+export const getProfile = async ({userId, friendId, supabaseClient}: SelectProfileType) => {
+  // Check if they are a friend of the user
+  const check = await checkIsFriends({supabaseClient, userId, friendId})
+  if (check == false) {
+    throw new Error("Not Friend or Not Accepted Yet")
+  }
+
+  const { data: ratingData, error: ratingDataFetchError} = await supabaseClient
+    .from("Ratings")
+    .select("film_id, rating, note, film_name")
+    .eq("user_id", userId)
+  
+  if (ratingDataFetchError) {
+    throw new Error("Failed to Fetch Rating Data")
+  }
+
+  const { data: userMetadata, error: userMetadataFetchError } = await supabaseClient
+    .from("User_Profiles")
+    .select("genres, movies")
+    .eq("user_id", userId)
+    .single()
+  
+  if (userMetadataFetchError) {
+    throw new Error("Failed to Fetch Metadata")
+  }
+
+  return { ratingData, userMetadata}
 }
--- service/query/queryService.ts
+++ service/query/queryService.ts
@@ -1,34 +1,33 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
 
+// Only Select Films Here
 type SelectFilmRequestType = {
   supabaseClient: SupabaseClient
 };
 
-type DeleteFilmRequestType = {
-  supabaseClient: SupabaseClient
-};
+export const bookmarkFilm = async ({supabase}) => {
 
-type RateFilmRequestType = {
-  supabaseClient: SupabaseClient
-  userId: string;
-  rating: number;
-  note: string;
 }
 
+// Get Recommended Films based on their Profile Embeddings
 export const getRecommendedFilms = async ({supabaseClient}: SelectFilmRequestType) => {
-  const response = supabaseClient.rpc("", )
+  const response = supabaseClient.rpc("", {
+
+  })
   return response
 }
 
 // Get All Films
 export const getFilms = async ({ supabaseClient }: SelectFilmRequestType) => {
-  const { data: filmData, error: filmError } = await supabaseClient.from("").select();
+  // Let them search by 
+  const { data: filmData, error: filmError } = await supabaseClient
+    .from("Films")
+    .select("title, cast, id, director, genre, poster_url, release_year")
+    .limit(100);
+
   if (filmError) {
     throw new Error("Failed to Get Films");
   }
+
   return filmData;
 };
-
-
-export const deleteFilms = async ({supabaseClient}: DeleteFilmRequestType) => {
-} 
