2089f87f9714fb9030013684f2d9090ce4d921d3
Jie Xuan Liu <baronliu1993@gmail.com>
2026-01-10 23:55:08 -0500

    implemented supabase rpc

--- service/query/queryService.ts
+++ service/query/queryService.ts
@@ -1,32 +1,59 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
+import type { UUID } from "node:crypto";
 
 const TMDB_API_BASE = process.env.TMDB_API_BASE;
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
 
 // Only Select Films Here
-type SelectFilmRequestType = {
+type SelectRelatedFilmRequestType = {
   genres: string;
   countries: string;
 };
 
+interface SelectRecommendedFilmRequestType {
+  supabaseClient: SupabaseClient;
+  userId: UUID;
+}
 
+interface SelectFriendsFilmRequestType
+  extends SelectRecommendedFilmRequestType {}
 
+type BookmarkRequestType = {};
 
-export const bookmarkFilm = async ({}) => {
-
-};
+export const bookmarkFilm = async ({}) => {};
 
 // Get Recommended Films based on their Profile Embeddings
 export const getRecommendedFilms = async ({
-
-}: SelectFilmRequestType) => {
-
+  supabaseClient,
+  userId,
+}: SelectRecommendedFilmRequestType) => {
+  const { data, error } = await supabaseClient.rpc("get_recommended", {
+    user_id: userId
+  })
+  if (error) {
+    throw new Error("Failed to Fetch Recommended")
+  }
+  return data
 };
 
-// Get All Films
+// Check Films Friends are Watching or Rating
+export const getFriendFilms = async ({supabaseClient, userId}: SelectFriendsFilmRequestType) => {
+  const {data, error }= await supabaseClient.rpc("", {
+    user_id: userId
+  })
+
+  if (error) {
+    throw new Error("Faield To Fetch Friend Films")
+  }
+
+  return data
+};
+
+// Get Films For Search Feature To Get Started
 export const getRelatedFilms = async ({
   genres,
   countries,
-}: SelectFilmRequestType) => {
+}: SelectRelatedFilmRequestType) => {
   const fetchSearches = await fetch(
     `${TMDB_API_BASE}/3/discover/movie?with_genres=${genres}&with_origin_country=${countries}&page=1`,
     {
