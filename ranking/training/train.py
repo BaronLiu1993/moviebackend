2631926056cd4b4286a5eddc1c2a243ec199be99
Jie Xuan Liu <baronliu1993@gmail.com>
2026-02-15 00:10:39 -0500

    starting the training pipeline

---
+++ ranking/training/train.py
@@ -0,0 +1,8 @@
+import lightgbm as lgb
+import pandas as pd
+import numpy as np
+
+# Train a LightGBM model using the training data
+# Interaction Score, User Total Interactions, User 
+def train_model():
+    pass    
