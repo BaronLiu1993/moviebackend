45fe7a8c177d056c632c8356c2e3cd8fe1ab2a21
Jie Xuan Liu <baronliu1993@gmail.com>
2026-02-26 23:10:43 -0500

    implemented the analytics router impression insertion

--- router/analytics/analyticsRouter.ts
+++ router/analytics/analyticsRouter.ts
@@ -5,25 +5,34 @@ import {
 } from "../../service/analytics/analyticsService.js";
 import { verifyToken } from "../../middleware/verifyToken.js";
 import { validateZod } from "../../middleware/schemaValidation.js";
-import { bookmarkRequestSchema, likeRequestSchema, bulkImpressionsRequestSchema } from "../../schemas/analyticsSchema.js";
+import {
+  bookmarkRequestSchema,
+  likeRequestSchema,
+  bulkImpressionsRequestSchema,
+} from "../../schemas/analyticsSchema.js";
+import impressionQueue from "../../queue/impression/addImpressionQueue.js";
 
 const router = Router();
 
-router.post("/bookmark", validateZod(bookmarkRequestSchema), async (req, res) => {
-  const { userId, tmdbId, film_name, genre_ids } = req.body;
-  try {
-    await handleBookmark({
-      userId,
-      tmdbId,
-      film_name,
-      genre_ids,
-    });
-    return res.status(200).send();
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: "Internal Server Error" });
-  }
-});
+router.post(
+  "/bookmark",
+  validateZod(bookmarkRequestSchema),
+  async (req, res) => {
+    const { userId, tmdbId, film_name, genre_ids } = req.body;
+    try {
+      await handleBookmark({
+        userId,
+        tmdbId,
+        film_name,
+        genre_ids,
+      });
+      return res.status(200).send();
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+);
 
 router.post("/like", validateZod(likeRequestSchema), async (req, res) => {
   const { userId, tmdbId, film_name, genre_ids } = req.body;
@@ -41,16 +50,26 @@ router.post("/like", validateZod(likeRequestSchema), async (req, res) => {
   }
 });
 
-router.post("/bulk-impressions", verifyToken, validateZod(bulkImpressionsRequestSchema), async(req, res) => {
+router.post(
+  "/bulk-impressions",
+  verifyToken,
+  validateZod(bulkImpressionsRequestSchema),
+  async (req, res) => {
     const { impressions } = req.body;
 
     try {
-        await handleImpression(impressions);
-        return res.status(200).send();
+      await impressionQueue.addBulk(
+        impressions.map((imp: any) => ({
+          name: "impression-sync",
+          data: imp,
+        })),
+      );
+      return res.status(200).send();
     } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: "Internal Server Error" });
+      console.log(err);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
-})
+  },
+);
 
 export default router;
